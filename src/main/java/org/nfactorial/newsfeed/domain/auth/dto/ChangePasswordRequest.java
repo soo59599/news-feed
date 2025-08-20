509c24a86313023953aa1ce1e779ccf14d3fee82
Youngguan Yu <157783368+VBKOROA@users.noreply.github.com>
2025-08-20 23:34:30 +0900

    fix: enhance ChangePasswordRequest validation

--- src/main/java/org/nfactorial/newsfeed/domain/auth/dto/ChangePasswordRequest.java
+++ src/main/java/org/nfactorial/newsfeed/domain/auth/dto/ChangePasswordRequest.java
@@ -1,13 +1,16 @@
 package org.nfactorial.newsfeed.domain.auth.dto;
 
+import org.nfactorial.newsfeed.domain.auth.AuthConstants;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record ChangePasswordRequest(
 	@NotBlank
 	String currentPassword,
-	@NotBlank
-	String changePassword
-) {
+	@NotBlank @Size(min = 8) @Pattern(regexp = AuthConstants.PASSWORD_PATTERN)
+	String changePassword) {
 }
