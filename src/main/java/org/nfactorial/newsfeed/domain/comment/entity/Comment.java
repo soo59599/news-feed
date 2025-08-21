4015baabf1fd4a099927a31c633b17422e72527c
Youngguan Yu <157783368+VBKOROA@users.noreply.github.com>
2025-08-21 14:28:09 +0900

    fix: Ensure Post and Profile relationships are mandatory in Comment entity

--- src/main/java/org/nfactorial/newsfeed/domain/comment/entity/Comment.java
+++ src/main/java/org/nfactorial/newsfeed/domain/comment/entity/Comment.java
@@ -12,6 +12,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -28,10 +29,12 @@ public class Comment extends BaseTimeEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(nullable = false)
 	private Post post;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(nullable = false)
 	private Profile profile;
 
 	@Column(nullable = false)
