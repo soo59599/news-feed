ff5f6d811184f1db5e6e1deac27ac7e208d091eb
abko <abko121115@gmail.com>
2025-08-25 10:17:15 +0900

    fix: Replace ProfileServiceApi with ProfileService in ProfileController.

--- src/main/java/org/nfactorial/newsfeed/domain/profile/controller/ProfileController.java
+++ src/main/java/org/nfactorial/newsfeed/domain/profile/controller/ProfileController.java
@@ -6,7 +6,7 @@ import org.nfactorial.newsfeed.common.security.AuthProfile;
 import org.nfactorial.newsfeed.common.security.AuthProfileDto;
 import org.nfactorial.newsfeed.domain.profile.dto.request.UpdateProfileCommand;
 import org.nfactorial.newsfeed.domain.profile.dto.response.ProfileResponse;
-import org.nfactorial.newsfeed.domain.profile.service.ProfileServiceApi;
+import org.nfactorial.newsfeed.domain.profile.service.ProfileService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,21 +19,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProfileController {
 
-	private final ProfileServiceApi profileServiceApi;
-
+	private final ProfileService profileService;
 
 	@GetMapping("/api/v1/profiles/{id}")
-	public GlobalApiResponse<ProfileResponse> getProfile(@PathVariable long profileId) {
-		ProfileResponse response = profileServiceApi.getProfileById(profileId);
+	public GlobalApiResponse<ProfileResponse> getProfile(@PathVariable("id")
+	long id) {
+		ProfileResponse response = profileService.getProfileById(id);
 		return GlobalApiResponse.of(SuccessCode.OK, response);
 	}
 
 	@PutMapping("/api/v1/profiles/me")
 	public GlobalApiResponse<ProfileResponse> updateMyProfile(
-		@RequestBody UpdateProfileCommand command,
-		@AuthProfile AuthProfileDto currentProfile)
-	{
-		ProfileResponse response = profileServiceApi.updateProfile(currentProfile.profileId(), command);
+		@RequestBody
+		UpdateProfileCommand command,
+		@AuthProfile
+		AuthProfileDto currentProfile) {
+		ProfileResponse response = profileService.updateProfile(currentProfile.profileId(), command);
 		return GlobalApiResponse.of(SuccessCode.OK, response);
 	}
 }
